3e5153fa88196c797845aa3559b5ee7a33b4dd35
haopeng jiang <haopengjiang4work@gmail.com>
2021-08-14 16:18:06 -0400

    first commit for orbiseed code test

---
+++ utils/utils.js
@@ -0,0 +1,17 @@
+module.exports.findValueWithMinLabel = function(arr) {
+	return parseInt(arr.map(str => (
+		{ 
+			type: str.split(":")[0],
+			value: str.split(":")[1]
+		}
+	)).find(obj => obj.type === "min").value);
+}
+
+module.exports.findValueWithMaxLabel = function(arr) {
+	return parseInt(arr.map(str => (
+		{ 
+			type: str.split(":")[0],
+			value: str.split(":")[1]
+		}
+	)).find(obj => obj.type === "max").value);
+}
